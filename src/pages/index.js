7a4633de2d2b02c2138ac3474e3728cd5d00e271
Nick Carchedi <nick.carchedi@gmail.com>
2020-12-10 14:36:22 -0700

    remove resume link

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,8 +57,8 @@ export default function Home() {
     <>
       <SEO title="Home" />
       <div className="px-10 py-6 md:px-20 md:py-12 lg:py-16 xl:px-24">
-        <h1 className="font-serif font-bold mb-6 text-5xl sm:text-6xl md:text-7xl md:mb-10 xl:text-8xl">
-          <span className="-ml-2 inline-block animate-wave origin-nearly-bottom-right">
+        <h1 className="mb-6 font-serif text-5xl font-bold sm:text-6xl md:text-7xl md:mb-10 xl:text-8xl">
+          <span className="inline-block -ml-2 animate-wave origin-nearly-bottom-right">
             <Emoji symbol="👋" label="wave" />
           </span>{" "}
           I'm Nick
@@ -73,32 +73,22 @@ export default function Home() {
             />
           ))}
         </div>
-        <div className="text-2xl max-w-screen-lg sm:text-3xl md:text-4xl md:leading-snug md:font-light lg:text-5xl">
+        <div className="max-w-screen-lg text-2xl sm:text-3xl md:text-4xl md:leading-snug md:font-light lg:text-5xl">
           <p className="mb-5">
             I'm a self-taught{" "}
-            <span className="text-red-700 bg-red-200 px-1">developer</span> with
+            <span className="px-1 text-red-700 bg-red-200">developer</span> with
             experience as a{" "}
-            <span className="text-indigo-700 bg-indigo-200 px-1">
+            <span className="px-1 text-indigo-700 bg-indigo-200">
               technical product manager
             </span>
             ,{" "}
-            <span className="text-green-700 bg-green-200 px-1">
+            <span className="px-1 text-green-700 bg-green-200">
               startup executive
             </span>{" "}
             and{" "}
-            <span className="text-orange-700 bg-orange-200 px-1">
+            <span className="px-1 text-orange-700 bg-orange-200">
               data scientist
-            </span>
-            . Here's my{" "}
-            <a
-              className="border-red-700 hover:border-red-200 border-b-4 md:border-b-6 xl:border-b-8"
-              href="https://www.notion.so/Nick-Carchedi-f18aebc1c07e4ba38eac62a89c90ac7b"
-              target="_blank"
-              rel="noreferrer"
-            >
-              resume
-            </a>
-            .
+            </span>.
           </p>
           <p>
             Let's solve some problems together{" "}
@@ -112,11 +102,11 @@ export default function Home() {
             target="_blank"
             rel="noreferrer"
           >
-            <div className="cta px-4 py-2 text-3xl bg-red-700 text-white cursor-pointer rounded sm:text-4xl md:px-6 md:py-3 lg:px-8">
+            <div className="px-4 py-2 text-3xl text-white bg-red-700 rounded cursor-pointer cta sm:text-4xl md:px-6 md:py-3 lg:px-8">
               Hire Me
             </div>
           </a>
-          <span className="animate-point ml-4 text-6xl md:text-7xl">
+          <span className="ml-4 text-6xl animate-point md:text-7xl">
             <Emoji symbol="👈" label="point left" />
           </span>
         </div>
